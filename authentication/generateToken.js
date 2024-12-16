a532f8838224ee1a9ddbccf1c00c75c5e1d12aef
sobhik-sawdagar <sobhiksawdagar@gmail.com>
2024-12-16 23:20:49 +0530

    minor update in the generateToken file

--- authentication/generateToken.js
+++ authentication/generateToken.js
@@ -1,4 +1,6 @@
 const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv'); //import the dotenv module
+dotenv.config(); //configure the dotenv module
 
 const generateToken = (payload) => { //function to generate a jwt token
     return jwt.sign(payload, process.env.JWT_SECRET, {expiresIn: '1d'}); //sign the payload with the secret key and set the expiry time to 1 day
